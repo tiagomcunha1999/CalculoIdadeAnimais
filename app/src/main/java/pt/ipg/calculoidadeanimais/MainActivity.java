c9c95abebd0040dffa967433e8dae0dfe16c3ba0
Tiago Cunha <1700597@sal.ipg.pt>
2020-06-14 18:33:05 +0100

    App concluida

--- app/src/main/java/pt/ipg/calculoidadeanimais/MainActivity.java
+++ app/src/main/java/pt/ipg/calculoidadeanimais/MainActivity.java
@@ -3,12 +3,31 @@ package pt.ipg.calculoidadeanimais;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText editIdade;
+    private TextView Resultado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editIdade = findViewById(R.id.editIdade);
+        Resultado = findViewById(R.id.Resultado);
+    }
+
+    public void calcularIdade (View view){
+
+        int idade = Integer.parseInt(editIdade.getText().toString());
+
+        int resultado = idade*7;
+
+        Resultado.setText("O seu animal tem: " + resultado + " anos");
+
+
+
     }
 }
